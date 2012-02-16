a6dcccb21d1e994142cfbfab566f8618e818b7fc
Administrator <admin@ebrosnan-lt.local>
2012-02-16 22:23:50 +1100

    Not using printers fact in provider because arrays are not recommended as facts. Left printers fact in the package for the moment. Fleshed out the cups provider which is now working with a limited set of options. Added a new test manifest.

--- lib/facter/printers.rb
+++ lib/facter/printers.rb
@@ -1,11 +1,12 @@
-# Get a list of printers which are installed and enabled for printing from CUPS
+# Create a fact which generates a list of comma separated printers
 
 Facter.add(:printers) do
   setcode do
 
-    %x{/usr/bin/lpstat -p}.split("\n").map do |line|
-      line.match(/printer (.*) is/)[0]
+    printers = %x{/usr/bin/lpstat -p}.split("\n").map do |line|
+      line.match(/printer (.*) is/).captures[0]
     end
-  end
 
+    printers.join(',')
+  end
 end
--- lib/puppet/provider/printer/cups.rb
+++ lib/puppet/provider/printer/cups.rb
@@ -1,4 +1,7 @@
 # TODO: Consider enable/disable, accept/reject and options as property methods.
+# TODO: Consider parsing lpstat -l -p for more detail on prefetch
+# TODO: lpstat -v for device-uris in prefetch
+# TODO: change of device uri should imply destroy+create
 
 Puppet::Type.type(:printer).provide :cups, :parent => Puppet::Provider do
   desc "This provider manages installed printers using CUPS command line tools"
@@ -6,6 +9,7 @@ Puppet::Type.type(:printer).provide :cups, :parent => Puppet::Provider do
   commands :lpadmin => "/usr/sbin/lpadmin"
   commands :lpoptions => "/usr/bin/lpoptions"
   commands :lpinfo => "/usr/sbin/lpinfo"
+  commands :lpstat => "/usr/bin/lpstat"
 
   commands :cupsenable => "/usr/sbin/cupsenable"
   commands :cupsdisable => "/usr/sbin/cupsdisable"
@@ -13,29 +17,39 @@ Puppet::Type.type(:printer).provide :cups, :parent => Puppet::Provider do
   commands :cupsaccept => "/usr/sbin/cupsaccept"
   commands :cupsreject => "/usr/sbin/cupsreject"
 
+  # A hash of possible parameters to their command-line equivalents.
+  Cups_Options = {
+      :uri => '-v "%s"',
+      :description => '-D "%s"',
+      :location => '-L "%s"',
+      :ppd => '-P "%s"',
+      :enabled => '-E'
+  }
+
   class << self
 
-    # A hash of possible parameters to their command-line equivalents.
-    CUPS_KEYS_PARAMS = {
-        :name => '%s',
-        :uri => '-v "%s"',
-        :description => '-D "%s"',
-        :location => '-L "%s"',
-        :ppd => '-P "%s"',
-        :enabled => '-E',
-        :shared => '-o printer-is-shared=true',
-        :options => '-o %s=%s'
-    }
+    def printers
+      lpstat('-p').split("\n").map { |line|
+        line.match(/printer (.*) is/).captures[0]
+      }
+    end
+
+    def prefetch(resources)
+      printers.collect { |p|
+        new({ :name => p, :provider => :cups })
+      }
+    end
   end
 
   def create
+    options = Array.new
 
     # Handle most parameters via string substitution
-    params = @resource.keys.map do |k|
-      self.class.CUPS_KEYS_PARAMS[k] % @resource[k] if self.class.CUPS_KEYS_PARAMS.key?(k)
+    Cups_Options.keys.each do |k|
+      options.unshift Cups_Options[k] % @resource[k] if @resource.parameters.key?(k)
     end
 
-    lpadmin "-p", params
+    lpadmin "-p", @resource[:name], options
   end
 
   def destroy
@@ -43,6 +57,7 @@ Puppet::Type.type(:printer).provide :cups, :parent => Puppet::Provider do
   end
 
   def exists?
-    Facter["printers"].value.include?(@resource[:name])
+    #TODO: lpadmin considers printer names case-insensitive, use case-insensitive match
+    self.class.printers.include? @resource[:name]
   end
 end
