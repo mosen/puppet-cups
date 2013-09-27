77250ceb27fb048987e42c4134529f66c984f8a6
Daniel De Marco <ddm@bartol.udel.edu>
2013-09-27 16:31:05 -0400

    fix ENV

--- lib/facter/printers.rb
+++ lib/facter/printers.rb
@@ -3,7 +3,8 @@ require 'open3'
 Facter.add(:printers) do
   setcode do
 
-    stdin, stdout, stderr = Open3.popen3('LANG=C /usr/bin/lpstat -p')
+    ENV['LANG'] = 'C'
+    stdin, stdout, stderr = Open3.popen3('/usr/bin/lpstat -p')
     printers = stdout.read
     if stderr.read.empty?
       printers = printers.split("\n").map do |line|
