5844e25f4855ac0ba913b2d86c001b54f2b86f99
Tom Farwig <tmfarwig@ncsu.edu>
2016-05-16 14:09:12 -0400

    Changed service_name from $title to 'printer'

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,14 +2,15 @@
 class cups::config {
   if $::cups::cups_lpd_enable {    
     xinetd::service { 'cups-lpd' :
-      ensure      => 'present',
-      disable     => 'no',
-      socket_type => 'stream',
-      protocol    => 'tcp',
-      port        => '515',
-      wait        => 'no',
-      user        => 'lp',
-      server      => '/usr/lib/cups/daemon/cups-lpd',  
+      ensure       => 'present',
+      service_name => 'printer',
+      disable      => 'no',
+      socket_type  => 'stream',
+      protocol     => 'tcp',
+      port         => '515',
+      wait         => 'no',
+      user         => 'lp',
+      server       => '/usr/lib/cups/daemon/cups-lpd',  
     }
   }
 }
